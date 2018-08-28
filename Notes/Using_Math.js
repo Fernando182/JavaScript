2e19ed2fd76b86ddf68f7ff7b3ea7a188bf69917
Fernando Hernandez <ffhf.1820@gmail.com>
2018-08-28 11:19:14 -0300

    Create Using_Math.js

---
+++ Notes/Using_Math.js
@@ -0,0 +1,17 @@
+// Get a random number between 0 and 1, use the Math.random() function.
+> console.log(Math.random());
+0.05374043523987404
+
+//If you want a random number between 1 and 10, multiply the results of Math.random by 10, then round up or down.
+
+//Use .floor to round down to a whole number:
+> console.log(Math.floor(Math.random() * 10));
+1
+
+//Use .ceil to round up to a whole number:
+console.log(Math.ceil(Math.random() * 10))
+6
+
+//Use .round to round to the nearest whole number:
+console.log(Math.round(Math.random() * 10))
+2
