47de14d1bdbfd99a05347f9ee9b45ee5a27f187e
Fernando Hernandez <ffhf.1820@gmail.com>
2019-10-13 20:46:38 -0300

    Update Grade F

--- 10_Days_of_Js/Day 2/Conditional_If-Else.js
+++ 10_Days_of_Js/Day 2/Conditional_If-Else.js
@@ -1,7 +1,9 @@
 function getGrade(score) {
     let grade;
     // Write your code here
-     if (score > 5 && score <= 10) {
+    if (score >= 0 && score <= 5) {
+        grade = "F";
+    } else if (score > 5 && score <= 10) {
         grade = "E";
     } else if (score > 10 && score <= 15) {
         grade = "D";
