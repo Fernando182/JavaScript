87f5f964b0ee447ac2ae1cbe50d21b9048845e40
Fernando Hernandez <ffhf.1820@gmail.com>
2019-10-13 21:10:51 -0300

    Update Conditional_Switch.js

--- 10_Days_of_Js/Day 2/Conditional_Switch.js
+++ 10_Days_of_Js/Day 2/Conditional_Switch.js
@@ -1,1 +1,35 @@
-//
+
+function getLetter(s) {
+    if (s.length < 1 || s.length > 100) {
+         return null;
+     }
+    let letter;
+    // Write your code here
+     switch(s.charAt(0).toLowerCase()) {
+          case "a":
+          case "e":
+          case "i":
+          case "o":
+          case "u":
+               letter="A"; 
+               break; 
+          case "b":
+          case "c":
+          case "d":
+          case "f":
+          case "g":
+               letter="B"; 
+               break;
+          case "h":
+          case "j":
+          case "k":
+          case "l":
+          case "m":
+               letter="C"; 
+               break; 
+          default: // everything else
+               letter="D";
+          break;  
+     } 
+    return letter;
+}
