eedb83a2920f78d393f303631f9e616cb385395f
Fernando Hernandez <ffhf.1820@gmail.com>
2019-10-14 23:10:06 -0300

    Update Arrays.js

--- 10_Days_of_Js/Day 3/Arrays.js
+++ 10_Days_of_Js/Day 3/Arrays.js
@@ -1,1 +1,11 @@
-//
+function getSecondLargest(nums) {
+    // Complete the function
+    // get the largest number
+    const max = Math.max(...nums);
+    
+    // filter out largest number 
+    nums = nums.filter(i => i !== max);
+    
+    // return new largest number 
+    return Math.max(...nums);
+}
