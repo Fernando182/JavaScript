53ec7092f31f3664d0ce45b80508e6cf10161c12
Fernando Hernandez <ffhf.1820@gmail.com>
2020-02-11 23:42:29 -0300

    Create  bubbleSortProto.js

---
+++ Algorithms_Js/bubbleSortProto.js
@@ -0,0 +1,18 @@
+function bubbleSort() {
+      const len = this.length;
+      const inputArray = this;
+      for (let i=0; i < len; i++) {
+            for (let j=0; j < len - i; j++) {
+                  if(inputArray[j] > inputArray[j+1]) {
+                        const tmp = inputArray[j];
+                        inputArray[j] = inputArray[j+1];
+                        inputArray[j + 1] = tmp;
+                  }
+
+            }
+      }
+      return inputArray;
+}
+Array.prototype.bubbleSort = bubbleSort;
+
+console.log([6,0,2,4,5].bubbleSort());
