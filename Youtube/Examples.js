7c4a96c07f9261f9b5b227e37d7a7b4b382b8f05
FernandoFH <ffhf.1820@gmail.com>
2020-09-21 23:54:56 -0300

    UPDATE: Examples js

--- Youtube/Examples.js
+++ Youtube/Examples.js
@@ -7,9 +7,28 @@ const multiply = (a , b) => {
     for (i = 0 ;i < Math.abs(b); i++){
         result = positive ? result + a : result - a
     }
-    
+
     return result
 }
 
-const a = multiply(-5,5)
+const a = multiply (5, -3)
 console.log(a)
+
+// Obtener numero mayor de un array con una sola iteracion 
+
+const getBiggest = (arr) => arr.reduce((acc, el) => acc > el ? acc : el) 
+
+const b = getBiggest([50, 100, 200, -544, 458, 1000])
+console.log(b)
+
+//  iterando una sola vez un arreglo eliminar Undifene,  false,  0 y null
+
+const clean = (arr) => arr.reduce((acc, el) => {
+    if (el) {
+        acc.push(el)
+    }
+    return acc
+}, []) 
+
+const c = clean([1, undefined, null, 0, 2])
+console.log(c)
